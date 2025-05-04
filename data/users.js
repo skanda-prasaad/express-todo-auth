a307ff7c397154386f2316f95558baafc3041e66
Skanda Prasad <skandaprasad595@gmail.com>
2025-05-04 23:54:20 +0530

    done auth.js

--- data/users.js
+++ data/users.js
@@ -1,22 +1,24 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const users = path.join(__dirname , 'users.json');
+const users = path.join(__dirname, "users.json");
 
-async function getUser(user){
-    try{
-        const data = fs.readFile(users, 'utf-8', function(rej, res){
-            // why no logic
-        });
-    }catch(err){
-        
-    }
+async function getUser() {
+  try {
+    const data = await fs.readFile(users, "utf-8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error("Error reading user file:", err);
+    return [];
+  }
 }
 
-function saveUser(user){
-    fs.writeFile(users, JSON.stringify(user, null, 2), 'utf-8', function(err){
-        // why no function called
-    })
+async function saveUser(user) {
+  try {
+    await fs.writeFile(users, JSON.stringify(users, null, 2), "utf-8");
+  } catch (err) {
+    console.error("Error saving users file:", err);
+  }
 }
 
-module.exports = {getUser , saveUser};
+module.exports = { getUser, saveUser };
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,58 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { getUsers, saveUsers } = require("../data/users");
+const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post('/signup', function(req, res){
-    const {username , password} = req.body;
-    if(!username || !password){
-        res.status(400).send("Username and password are required...");
-    }
-})
+router.post("/signup", async function (req, res) {
+  let users = [];
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ msg: "Username and password are required..." });
+  }
+  users = await getUsers();
+  const userExist = users.find((user) => user.username == username);
+  if (userExist) {
+    return res.status(409).json({ message: "User already exists" });
+  }
+  const hashPsw = await bcrypt.hash(password, 10);
+  users.push({
+    username: username,
+    password: hashPsw,
+  });
+  await saveUsers(users);
+
+  res.status(201).json({
+    message: "User registered successfully",
+  });
+});
+
+// login route :
+
+router.post("/login", async function (req, res) {
+  let users = [];
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ msg: "Username and password are required..." });
+  }
+
+  users = await getUsers();
+  const existUser = users.find((user) => user.username == username);
+
+  if (!existUser) {
+    return res.status(401).json({ message: "Credentials incorrect" });
+  }
+  const match = await bcrypt.compare(password, existUser.password);
+  if (!match) {
+    return res.status(401).json({ message: "Credentials incorrect" });
+  }
+  const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: "1h" });
+  res.json({ token });
+});
+
+module.exports = router;
