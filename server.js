7857c10a7a7bf6a23e61b4202b17d53f28edf3b5
Skanda Prasad <skandaprasad595@gmail.com>
2025-05-06 22:41:35 +0530

    REBUILDING

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.use("/todos", todoRoutes);
 
 // Handle all routes for SPA
 app.get("*", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 // Start server only if not in Vercel environment
 if (process.env.NODE_ENV !== "production") {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
 module.exports = app;
